5812d1aae1152ef58371c0e0ee0e9aac97e2a3f2
JackDAmico92 <jackd76@nycstudents.net>
2024-02-12 12:27:43 -0500

    cjames

--- greatest_common_factor.py
+++ greatest_common_factor.py
@@ -1,9 +1,11 @@
 print('2 numbers needed to find GCF')
+
 x = int(input('Number#1: '))
 y = int(input('Number#2: '))
 z = 0
-j = int
-def run(j):
- if x%(z+1) == 0 and y%(z+1) == 0:
-  print("hello")
-run(100)
+j = z+1
+def gcf(x,y):
+  if y%j == x%j:  
+   print('the gcf is:')
+   print(j)
+gcf(x,y)
