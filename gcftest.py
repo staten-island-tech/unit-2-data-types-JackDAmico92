a6241c5581dec3c00507a1b3cb4ab801c1d147c8
JackDAmico92 <jackd76@nycstudents.net>
2024-02-14 12:29:35 -0500

    changes :(

---
+++ gcftest.py
@@ -0,0 +1,16 @@
+print('2 numbers needed to find GCF')
+
+x = int(input('Number#1: '))
+y = int(input('Number#2: '))
+
+
+
+def numberguess():
+    while x%x - 1 != 0 and y%y - 1 != 0:
+        print('Checking')
+    for x % x - 1 == 0 and y % y - 1 == 0
+     
+
+numberguess()
+
+       
