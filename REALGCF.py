cc5b8535cfe418e816ba8f50e937b5691bd904d2
JackDAmico92 <jackd76@nycstudents.net>
2024-02-16 12:04:44 -0500

    Update REALGCF.py

--- REALGCF.py
+++ REALGCF.py
@@ -6,7 +6,7 @@ def factors(num):
      for i in range(1, num+1): 
         if num%i==0: 
              factorlist.append(i) 
-             return factorlist
+     return factorlist
 
 factors1 = factors(number1)
 factors2 = factors(number2)
@@ -15,5 +15,4 @@ for i in factors1[::-1]:
      if i in factors2:
         print(i)
         break
-
 
